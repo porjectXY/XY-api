27a6e2fa5c778b44c1add2ddd27d752683e91eb2
arano1212 <arano1212@gmail.com>
2024-07-22 01:53:01 -0600

    feat: add cors and morgan

--- api/api.js
+++ api/api.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
 import authRouter from './routes/authRoutes.js'
 import userRouter from './routes/userRoutes.js'
 import postRouter from './routes/postRoutes.js'
@@ -6,6 +8,8 @@ import searchRouter from './routes/searchRoutes.js'
 import notificationRouter from './routes/notificationRoutes.js'
 
 const api = express()
+api.use(cors())
+api.use(morgan('dev'))
 api.use(express.json())
 
 api.get('/test', (req, res) => {
