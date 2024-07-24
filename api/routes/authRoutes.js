bbfb4d757f68b2f78919f8e2b185517524d4ab72
arano1212 <arano1212@gmail.com>
2024-07-24 15:42:15 -0600

    feat: add check-username

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express'
-import { login, register, me } from '../controllers/authController.js'
+import { login, register, me, checkUserName } from '../controllers/authController.js'
 import { authUser } from '../middleWares/authValidator.js'
 
 const authRouter = Router()
 
 authRouter.post('/register', register)
 authRouter.post('/login', login)
+authRouter.get('/check-username/:username', checkUserName)
 
 authRouter.get('/me', authUser(), me)
 
