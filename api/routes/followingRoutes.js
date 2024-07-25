7b741436b23ca343090c10d7dccb21b20e07bb39
arano1212 <arano1212@gmail.com>
2024-07-25 01:46:06 -0600

    fix: changes

--- api/routes/followingRoutes.js
+++ api/routes/followingRoutes.js
@@ -4,42 +4,36 @@ import User from '../models/User.js'
 import mongoose from 'mongoose'
 import { authUser } from '../middleWares/authValidator.js'
 
-const followingRouter = Router()
+const followingRouter = Router({ mergeParams: true })
 
-// Obtener posts de los usuarios seguidos por el usuario logueado
 followingRouter.get('/followings/posts', authUser(), async (req, res) => {
   try {
-    // Obtener el ID del usuario logueado desde req.user.id
     const loggedInUserId = req.user.id
 
-    // Buscar el usuario logueado
     const loggedInUser = await User.findById(loggedInUserId).exec()
 
     if (!loggedInUser) {
       return res.status(404).json({ msg: 'User not found' })
     }
 
-    // Obtener los IDs de los usuarios seguidos
     const followingUserIds = loggedInUser.following
 
     if (followingUserIds.length === 0) {
       return res.json({ followingPosts: [] })
     }
 
-    // Convertir los IDs de los usuarios seguidos a ObjectId
     const followingUserObjectIds = followingUserIds.map(id => mongoose.Types.ObjectId(id))
 
-    // Buscar los posts de los usuarios seguidos y popular el campo userId con la información del usuario
     const followingPosts = await Post.find({ userId: { $in: followingUserObjectIds } })
-      .populate('userId', 'username avatar') // Ajusta los campos que quieres incluir del usuario
+      .populate('userId', 'username avatar')
       .exec()
 
     res.json({
       followingPosts: followingPosts.map(post => ({
         id: post._id,
-        userId: post.userId._id, // Aquí aseguramos que solo se incluya el ID del usuario
-        username: post.userId.username, // Incluimos el nombre de usuario del autor del post
-        avatar: post.userId.avatar, // Incluimos el avatar del autor del post
+        userId: post.userId._id,
+        username: post.userId.username,
+        avatar: post.userId.avatar,
         content: post.content,
         timestamp: post.timestamp,
         comments: post.comments,
